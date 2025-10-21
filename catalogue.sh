7e4d32944672aa8b3469610780f409e6abd4f524
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-21 15:10:03 +0530

    deployment

--- catalogue.sh
+++ catalogue.sh
@@ -44,7 +44,7 @@ if [ $? -ne 0 ]; then
     VALIDATE $? "Creating system user"
 else 
     echo -e "User already exist ... $Y SKIPPING $N"
-
+fi
 mkdir -p /app 
 VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
