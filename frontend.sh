06696c50944d77ef439dad00dab22ebca5e97dd7
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-10-22 17:34:19 +0530

    Deploying

--- frontend.sh
+++ frontend.sh
@@ -50,5 +50,5 @@ rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
-systemcrl restart nginx
+systemctl restart nginx
 VALIDATE $? "restarting Nginx"
